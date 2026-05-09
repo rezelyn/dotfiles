7bd4149dcc97fde170c1cadc12aa35dd7e8b83fa
Rezelyn <lu.aubry54@gmail.com>
2026-05-09 02:47:31 +0200

    feat: fish color support for tty-clock and unimatrix

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,5 +6,21 @@ if status is-interactive
     end
 end
 
+function clock
+    printf '\e]4;4;#afafff\e\\'
+    if contains -- -s $argv
+        tty-clock -C 4 -D -s
+    else
+        tty-clock -C 4 -D
+    end
+    printf '\e]104;4\e\\'
+end
+
+function matrix
+    printf '\e]4;4;#afafff\e\\'
+    unimatrix -c blue -s 95
+    printf '\e]104;4\e\\'
+end
+
 alias ls='exa -lahF --icons=always'
 alias grep='grep --color=auto'
